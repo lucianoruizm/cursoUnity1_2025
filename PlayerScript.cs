1dd799a8e11129446b039caa55e27da97bab95d6
Luciano Ruiz <lucianoruizm993@gmail.com>
2025-10-23 21:10:00 -0300

    Unity curso 1 - Primer commit (Movimiento de player, collision y camara referenciada al player)

---
+++ PlayerScript.cs
@@ -0,0 +1,86 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    private float speed = 15f;
+    public float MinSpeed = 15f;
+    public float MaxSpeed = 25f;
+
+    public float JumpForce = 5f;
+
+    // Camara
+    public float Sensibility = 2f;
+    public float LimitX = 45;
+    public Transform cam;
+
+    private float rotationX;
+    private float rotationY;
+
+    public bool IsGrounded;
+
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movimiento
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = MaxSpeed;
+        }
+        else
+        {
+            speed = MinSpeed;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Jump();
+        }
+
+        transform.Translate(new Vector3(x, 0, y) * Time.deltaTime * speed);
+
+        // CAMARA
+        rotationX += -Input.GetAxis("Mouse Y") * Sensibility;
+        rotationX = Mathf.Clamp(rotationX, -LimitX, LimitX);
+        cam.localRotation = Quaternion.Euler(rotationX, 0, 0);
+        transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * Sensibility, 0);
+    }
+
+    public void Jump()
+    {
+        if (IsGrounded)
+        {
+            rb.AddForce(new Vector3(0, JumpForce, 0), ForceMode.Impulse);
+        }
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            IsGrounded = true;
+        }
+    }
+
+    public void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            IsGrounded = false;
+        }
+    }
+}
